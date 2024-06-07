d5b443c54671d31ba6ba61e35236bcdff727fd5a
Yrr3L <96141560+Yrr3L@users.noreply.github.com>
2024-06-08 04:44:57 +0800

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,9 @@
-import { getSinglePost } from "../lib/notion";
+import { getSinglePost } from "@/app/lib/notion";
 import Link from "next/link";
 
-export default function Page({ post }) {
+export default async function Page({ params }: { params: { slug: string } }) {
+  const post = await fetchBlogData(params.slug);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-[672px] mx-auto">
@@ -9,34 +11,20 @@ export default function Page({ post }) {
           &lt; Go Back
         </Link>
         <div className="my-4">
-          <span className="text-gray-600">
-            {post.publishedDate} | By {post.author}{" "}
-          </span>
-        </div>
+		  <span className="text-gray-600">
+		    {post.publishedDate} | By {post.author}{" "}
+		  </span>
+		</div>
         <h1 className="text-6xl text-gray-900 font-bold my-6">{post.title}</h1>
-        <div className="prose" dangerouslySetInnerHTML={{ __html: post.content }}></div>
+        <div
+          className="prose"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        ></div>
       </div>
     </main>
   );
 }
 
-export async function getStaticPaths() {
-  // Generate paths based on data from API
-  // Example:
-  // const paths = [{ params: { slug: 'first-post' }}, { params: { slug: 'second-post' }}];
-  // return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params }) {
-  const post = await fetchBlogData(params.slug);
-  return {
-    props: {
-      post,
-    },
-  };
-}
-
-async function fetchBlogData(slug) {
-  const res = await getSinglePost(slug);
+async function fetchBlogData(slug: string) {
+  const res = getSinglePost(slug);
   return res;
-}
