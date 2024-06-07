3aa38676b41984fa5865ce53a930999739ade9a5
Yrr3L <96141560+Yrr3L@users.noreply.github.com>
2024-06-08 04:53:44 +0800

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,7 @@
 import { getSinglePost } from "@/app/lib/notion";
 import Link from "next/link";
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const post = await fetchBlogData(params.slug);
-
+export default function Page({ post }: { post: Post }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-[672px] mx-auto">
@@ -11,20 +9,27 @@ export default async function Page({ params }: { params: { slug: string } }) {
           &lt; Go Back
         </Link>
         <div className="my-4">
-		  <span className="text-gray-600">
-		    {post.publishedDate} | By {post.author}{" "}
-		  </span>
-		</div>
+          <span className="text-gray-600">
+            {post.publishedDate} | By {post.author}{" "}
+          </span>
+        </div>
         <h1 className="text-6xl text-gray-900 font-bold my-6">{post.title}</h1>
-        <div
-          className="prose"
-          dangerouslySetInnerHTML={{ __html: post.content }}
-        ></div>
+        <div className="prose" dangerouslySetInnerHTML={{ __html: post.content }}></div>
       </div>
     </main>
   );
 }
 
+export async function getServerSideProps({ params }) {
+  const post = await fetchBlogData(params.slug);
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
 async function fetchBlogData(slug: string) {
-  const res = getSinglePost(slug);
+  const res = await getSinglePost(slug);
   return res;
+}
