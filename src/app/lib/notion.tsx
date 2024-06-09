2cd4df57947d1324944c790fcd4b5c5849a2a1e1
Yrr3L <96141560+Yrr3L@users.noreply.github.com>
2024-06-09 18:15:14 +0800

    Update notion.tsx

--- src/app/lib/notion.tsx
+++ src/app/lib/notion.tsx
@@ -1,4 +1,4 @@
-import { NotionRenderer } from "@notion-render/client";
+import { NotionRenderer } from "@notion-render/client"; 
 const { Client } = require("@notionhq/client");
 
 export const notion = new Client({
@@ -24,52 +24,43 @@ export const getAllPublishedBlog = async () => {
 
   const allPosts = posts.results;
 
-  // Fetch full content for each post
-  const detailedPosts = await Promise.all(allPosts.map(async (post: any) => {
-    const content = await getPageContent(post);
-    return content;
-  }));
-
-  return detailedPosts;
+  return allPosts.map((post: any) => {
+    return getPageContent(post);
+  });
 };
 
 export const getSinglePost = async (slug: string) => {
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
-    filter: {
-      property: "slug",
-      formula: {
-        string: {
-          equals: slug,
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID,
+      filter: {
+        property: "slug",
+        formula: {
+          string: {
+            equals: slug,
+          },
         },
       },
-    },
-  });
-
-  const singlePost = response.results[0];
-
-  const responseBlockPages = await notion.blocks.children.list({
-    block_id: singlePost.id,
-  });
-
-  const content = responseBlockPages.results;
-  const renderer = new NotionRenderer({
-    client: notion,
-  });
-  const html = await renderer.render(...content);
-
-  return {
-    ...getPageContent(singlePost),
-    content: html,
+    });
+  
+    const singlePost = response.results[0];
+  
+    const responseBlockPages = await notion.blocks.children.list({
+      block_id: singlePost.id,
+    });
+  
+    const content = responseBlockPages.results;
+    const renderer = new NotionRenderer({
+      client: notion,
+    });
+    const html = await renderer.render(...content);
+  
+    return {
+      ...getPageContent(singlePost),
+      content: html,
+    };
   };
-};
 
-const getPageContent = async (post: any) => {
-  const responseBlockPages = await notion.blocks.children.list({
-    block_id: post.id,
-  });
-
-  const content = responseBlockPages.results;
+const getPageContent = (post: any) => {
   return {
     id: post.id,
     title: post.properties.title.title[0].plain_text,
@@ -77,6 +68,5 @@ const getPageContent = async (post: any) => {
     publishedDate: post.properties.publishedDate.date.start,
     slug: post.properties.slug.formula.string,
     description: post.properties.description.rich_text[0].plain_text,
-    blocks: content, // include content blocks
   };
 };
